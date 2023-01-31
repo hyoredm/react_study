902a4ebe49d8cf7c33d34721d33849c31f08f720
hyoredm <hyoredm@gmail.com>
2023-02-01 00:02:18 +0900

    commit

--- usejs.jsx
+++ usejs.jsx
@@ -6,11 +6,11 @@ function formatName(user) {
      lastName: 'Lee'
     };
     
-    const element = {
+    const element = (
         <h1>
         Hello, {formatUser(user)}
         </h1>
-        } ;// html 코드 사이에 괄호를 사용하여 변수가 아닌 자바스크립트 함수를 호출
+     ) ;// html 코드 사이에 괄호를 사용하여 변수가 아닌 자바스크립트 함수를 호출
    
    ReactDOM.render(
     element,
